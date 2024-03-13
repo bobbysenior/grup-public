2063f8f5c64014843682658acbef7b2865d1e6bd
bobbysenior <thomezino@gmail.com>
2024-03-13 15:28:04 +0400

    v0.31

--- web_app.py
+++ web_app.py
@@ -5,6 +5,13 @@ template_dir = "/home/bobby/Projet/grup-public/static"
 
 app = Flask(__name__, template_folder=template_dir)
 
+#For the pump, True means powered and False means not powered
+#For the valves, True means open and False means closed
+pump_state = False
+valve1_state = False
+valve2_state = False
+
+
 @app.route("/")
 def home():
     return render_template("Home/home.html")
@@ -18,22 +25,30 @@ def dashboard():
     water_level = request.args.get('water_level') #From 0 to 100
     if water_level == None:
         water_level = 0
-    return render_template("Dashboard/dashboard.html", water_level=water_level)
+    return render_template("Dashboard/dashboard.html", water_level=water_level, pump_state=pump_state, valve1_state=valve1_state, valve2_state=valve2_state)
 
 
 @app.route("/control", methods=['POST', 'GET'])
 def control():
+    global pump_state, valve1_state, valve2_state
     # Check if the request contains JSON data
+    if request.method == 'GET':
+        return f"Pompe : {pump_state} || Valve 1 : {valve1_state} || Valve 2 : {valve2_state}"
     if request.is_json:
         data = request.get_json()  # Get JSON data from the request
         # Assuming JSON data contains a 'name' field
         name = data.get('name')
         # Process the data (here we'll just echo it back)
+        if data.get('buttonId') == 'button_pump':
+            pump_state = not pump_state
+        elif data.get('buttonId') == 'button1':
+            valve1_state = not valve1_state
+        elif data.get('buttonId') == 'button2':
+            valve2_state = not valve2_state
         response_data = {'message': f'Hello, {name}! Your POST request was successful.'}
         return jsonify(response_data), 200  # Return a JSON response with a 200 status code
     else:
         return jsonify({'error': 'Request must contain JSON data.'}), 400  # Return an error if request doesn't contain JSON 
-    return "Aled"
 
 if __name__ == "__main__":
     #app.run(debug=False)
